6298141dbc16c70c1d7df0503f6bdcc06c5908a1
Fabian F Groß <fabian.f.gross@deutschebahn.com>
2022-08-08 13:21:17 +0200

    test: use hamcrest for dict assertions

--- reader/chart_reader_test.py
+++ reader/chart_reader_test.py
@@ -1,7 +1,6 @@
 import chart_reader
-import yaml
 from unittest.mock import Mock
-import logging
+from hamcrest import assert_that, has_entries, contains_inanyorder
 
 
 def test_read():
@@ -19,19 +18,59 @@ def test_read():
         "commands": [],
     }
     result = chart_reader.read("test/test-chart")
-    assert result is {'name': 'test-chart', 'appVersion': '1.16.0', 'apiVersion': 'v2', 'version': '0.1.0', 'description': 'A Helm chart for Kubernetes', 'type': 'application', 'dependencies': [{'name': 'postgresql', 'condition': 'postgresql.enabled', 'version': '1.2.3', 'repository': 'https://lol.de/repo/'}, {'name': 'mysql', 'condition': 'mysql.enabled', 'version': '1.2.3', 'repository': 'https://lol.de/repo/'}], 'values': [{'name': 'replicaCount', 'description': 'how many replicas to deploy\n', 'default': 1, 'example': ''}, {'name': 'image', 'description': 'which image to deploy\n', 'default': {'repository': 'nginx', 'pullPolicy': 'IfNotPresent', 'tag': ''}, 'example': '\nimage:\n  repository: very-doge-wow/stella\n  pullPolicy: IfNotPresent\n  tag: "latest"\n'}, {'name': 'imagePullSecrets', 'description': '', 'default': [], 'example': ''}, {'name': 'nameOverride', 'description': '', 'default': '', 'example': ''}, {'name': 'fullnameOverride', 'description': '', 'default': '', 'example': ''}, {'name': 'serviceAccount', 'description': '', 'default': {'create': True, 'annotations': {}, 'name': ''}, 'example': ''}, {'name': 'podAnnotations', 'description': '', 'default': {}, 'example': ''}, {'name': 'podSecurityContext', 'description': '', 'default': {}, 'example': ''}, {'name': 'securityContext', 'description': '', 'default': {}, 'example': ''}, {'name': 'service', 'description': '', 'default': {'type': 'ClusterIP', 'port': 80}, 'example': ''}, {'name': 'ingress', 'description': '', 'default': {'enabled': False, 'className': '', 'annotations': {}, 'hosts': [{'host': 'chart-example.local', 'paths': [{'path': '/', 'pathType': 'ImplementationSpecific'}]}], 'tls': []}, 'example': ''}, {'name': 'resources', 'description': '', 'default': {}, 'example': ''}, {'name': 'autoscaling', 'description': '', 'default': {'enabled': False, 'minReplicas': 1, 'maxReplicas': 100, 'targetCPUUtilizationPercentage': 80}, 'example': ''}, {'name': 'nodeSelector', 'description': '', 'default': {}, 'example': ''}, {'name': 'tolerations', 'description': '', 'default': [], 'example': ''}, {'name': 'affinity', 'description': '', 'default': {}, 'example': ''}], 'templates': [{'path': 'deployment.yaml'}, {'path': 'ingress.yaml'}, {'path': 'service.yaml'}, {'path': 'hpa.yaml'}, {'path': 'serviceaccount.yaml'}], 'objects': [{'kind': 'Deployment', 'from Template': 'deployment.yaml'}, {'kind': 'Ingress', 'from Template': 'ingress.yaml'}, {'kind': 'Service', 'from Template': 'service.yaml'}, {'kind': 'HorizontalPodAutoscaler', 'from Template': 'hpa.yaml'}, {'kind': 'ServiceAccount', 'from Template': 'serviceaccount.yaml'}], 'commands': [{'description': '', 'command': ''}]}
+    assert_that(result, has_entries(
+        {'name': 'test-chart', 'appVersion': '1.16.0', 'apiVersion': 'v2', 'version': '0.1.0',
+         'description': 'A Helm chart for Kubernetes', 'type': 'application', 'dependencies': [
+            {'name': 'postgresql', 'condition': 'postgresql.enabled', 'version': '1.2.3',
+             'repository': 'https://lol.de/repo/'},
+            {'name': 'mysql', 'condition': 'mysql.enabled', 'version': '1.2.3', 'repository': 'https://lol.de/repo/'}],
+         'values': [
+             {'name': 'replicaCount', 'description': 'how many replicas to deploy\n', 'default': 1, 'example': ''},
+             {'name': 'image', 'description': 'which image to deploy\n',
+              'default': {'repository': 'nginx', 'pullPolicy': 'IfNotPresent', 'tag': ''},
+              'example': '\nimage:\n  repository: very-doge-wow/stella\n  pullPolicy: IfNotPresent\n  tag: "latest"\n'},
+             {'name': 'imagePullSecrets', 'description': '', 'default': [], 'example': ''},
+             {'name': 'nameOverride', 'description': '', 'default': '', 'example': ''},
+             {'name': 'fullnameOverride', 'description': '', 'default': '', 'example': ''},
+             {'name': 'serviceAccount', 'description': '', 'default': {'create': True, 'annotations': {}, 'name': ''},
+              'example': ''}, {'name': 'podAnnotations', 'description': '', 'default': {}, 'example': ''},
+             {'name': 'podSecurityContext', 'description': '', 'default': {}, 'example': ''},
+             {'name': 'securityContext', 'description': '', 'default': {}, 'example': ''},
+             {'name': 'service', 'description': '', 'default': {'type': 'ClusterIP', 'port': 80}, 'example': ''},
+             {'name': 'ingress', 'description': '', 'default': {'enabled': False, 'className': '', 'annotations': {},
+                                                                'hosts': [{'host': 'chart-example.local', 'paths': [
+                                                                    {'path': '/',
+                                                                     'pathType': 'ImplementationSpecific'}]}],
+                                                                'tls': []}, 'example': ''},
+             {'name': 'resources', 'description': '', 'default': {}, 'example': ''},
+             {'name': 'autoscaling', 'description': '',
+              'default': {'enabled': False, 'minReplicas': 1, 'maxReplicas': 100, 'targetCPUUtilizationPercentage': 80},
+              'example': ''}, {'name': 'nodeSelector', 'description': '', 'default': {}, 'example': ''},
+             {'name': 'tolerations', 'description': '', 'default': [], 'example': ''},
+             {'name': 'affinity', 'description': '', 'default': {}, 'example': ''}],
+         'templates': [{'path': 'deployment.yaml'}, {'path': 'ingress.yaml'}, {'path': 'service.yaml'},
+                       {'path': 'hpa.yaml'}, {'path': 'serviceaccount.yaml'}],
+         'objects': [{'kind': 'Deployment', 'from Template': 'deployment.yaml'},
+                     {'kind': 'Ingress', 'from Template': 'ingress.yaml'},
+                     {'kind': 'Service', 'from Template': 'service.yaml'},
+                     {'kind': 'HorizontalPodAutoscaler', 'from Template': 'hpa.yaml'},
+                     {'kind': 'ServiceAccount', 'from Template': 'serviceaccount.yaml'}],
+         'commands': [{'description': '', 'command': ''}]}
+    ))
 
 
 def test_generate_chart_metadata_real_file():
     result = chart_reader.generate_chart_metadata({}, "test/test-chart")
-    assert result is {
-        'apiVersion': 'v2',
-        'appVersion': '1.16.0',
-        'description': 'A Helm chart for Kubernetes',
-        'name': 'test-chart',
-        'type': 'application',
-        'version': '0.1.0'
-    }
+    assert_that(result, has_entries(
+        {
+            'apiVersion': 'v2',
+            'appVersion': '1.16.0',
+            'description': 'A Helm chart for Kubernetes',
+            'name': 'test-chart',
+            'type': 'application',
+            'version': '0.1.0'
+        }
+    ))
 
 
 def test_generate_chart_metadata_unknown():
@@ -40,14 +79,17 @@ def test_generate_chart_metadata_unknown():
     chart_reader.yaml.safe_load = Mock(return_value=mocked_content)
     result = chart_reader.generate_chart_metadata({}, "test/test-chart")
     chart_reader.yaml.safe_load = real_yaml_load
-    assert result is {
-        'apiVersion': 'unknown',
-        'appVersion': 'unknown',
-        'description': 'unknown',
-        'name': 'unknown',
-        'type': 'unknown',
-        'version': 'unknown'
-    }
+    assert_that(result, has_entries(
+        {
+            'apiVersion': 'unknown',
+            'appVersion': 'unknown',
+            'description': 'unknown',
+            'name': 'unknown',
+            'type': 'unknown',
+            'version': 'unknown'
+        }
+    ))
+
 
 def test_generate_values_doc_and_example():
     doc = {
@@ -64,7 +106,7 @@ def test_generate_values_doc_and_example():
         "commands": [],
     }
     result = chart_reader.generate_values_doc(doc, "test/values-stella")
-    assert result["values"] is [
+    assert_that(result["values"], contains_inanyorder(
         {
             'name': 'replicaCount',
             'description': 'how many replicas to deploy\n',
@@ -76,7 +118,7 @@ def test_generate_values_doc_and_example():
             'default': {'repository': 'nginx', 'pullPolicy': 'IfNotPresent', 'tag': ''},
             'example': '\nimage:\n  repository: very-doge-wow/stella\n  pullPolicy: IfNotPresent\n  tag: "latest"\n'
         }
-    ]
+    ))
 
 
 def test_generate_values_doc_only():
@@ -94,13 +136,13 @@ def test_generate_values_doc_only():
         "commands": [],
     }
     result = chart_reader.generate_values_doc(doc, "test/values-stella-only")
-    assert result["values"] is [
+    assert_that(result["values"], contains_inanyorder(
         {
             'name': 'replicaCount',
             'description': 'how many replicas to deploy\n',
             'default': 1, 'example': ''
         }
-    ]
+    ))
 
 
 def test_generate_requirements():
@@ -119,7 +161,7 @@ def test_generate_requirements():
     }
 
     result = chart_reader.generate_requirements(doc, "test/test-chart")
-    assert result["dependencies"] is [
+    assert_that(result["dependencies"], contains_inanyorder(
         {
             "name": "postgresql",
             "condition": "postgresql.enabled",
@@ -132,7 +174,7 @@ def test_generate_requirements():
             "version": "1.2.3",
             "repository": "https://lol.de/repo/"
         }
-    ]
+    ))
 
 
 def test_generate_templates():
@@ -151,24 +193,23 @@ def test_generate_templates():
     }
 
     result = chart_reader.generate_templates(doc, "test/test-chart")
-    assert result["templates"] is [
-            {
-                "path": "deployment.yaml"
-            },
-            {
-                "path": "ingress.yaml"
-            },
-            {
-                "path": "service.yaml"
-            },
-            {
-                "path": "hpa.yaml"
-            },
-            {
-                "path": "serviceaccount.yaml"
-            }
-        ]
-
+    assert_that(result["templates"], contains_inanyorder(
+        {
+            "path": "deployment.yaml"
+        },
+        {
+            "path": "ingress.yaml"
+        },
+        {
+            "path": "service.yaml"
+        },
+        {
+            "path": "hpa.yaml"
+        },
+        {
+            "path": "serviceaccount.yaml"
+        }
+    ))
 
 def test_generate_objects():
     doc = {
@@ -202,25 +243,26 @@ def test_generate_objects():
     }
 
     result = chart_reader.generate_objects(doc, "test/test-chart")
-    assert result["objects"] is [
-        {
-            "kind": "Deployment",
-            "from Template": "deployment.yaml"
-        },
-        {
-            "kind": "HorizontalPodAutoscaler",
-            "from Template": "hpa.yaml"
-        },
-        {
-            "kind": "Ingress",
-            "from Template": "ingress.yaml"
-        },
-        {
-            "kind": "Service",
-            "from Template": "service.yaml"
-        },
-        {
-            "kind": "ServiceAccount",
-            "from Template": "serviceaccount.yaml"
-        }
-    ]
+    assert_that(result["objects"], contains_inanyorder(
+            {
+                "kind": "Deployment",
+                "from Template": "deployment.yaml"
+            },
+            {
+                "kind": "HorizontalPodAutoscaler",
+                "from Template": "hpa.yaml"
+            },
+            {
+                "kind": "Ingress",
+                "from Template": "ingress.yaml"
+            },
+            {
+                "kind": "Service",
+                "from Template": "service.yaml"
+            },
+            {
+                "kind": "ServiceAccount",
+                "from Template": "serviceaccount.yaml"
+            }
+    ))
+
