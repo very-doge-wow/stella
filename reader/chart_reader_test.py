f1f136634a3b1fad7f837550b027acdb814f45e8
Fabian F Groß <fabian.f.gross@deutschebahn.com>
2022-08-08 13:37:27 +0200

    test: fix assertion

--- reader/chart_reader_test.py
+++ reader/chart_reader_test.py
@@ -18,43 +18,46 @@ def test_read():
         "commands": [],
     }
     result = chart_reader.read("test/test-chart")
+    result["objects"] = sorted(result["objects"], key=lambda item: item.get("kind"))
     assert_that(result, has_entries(
-        {'name': 'test-chart', 'appVersion': '1.16.0', 'apiVersion': 'v2', 'version': '0.1.0',
-         'description': 'A Helm chart for Kubernetes', 'type': 'application', 'dependencies': [
-            {'name': 'postgresql', 'condition': 'postgresql.enabled', 'version': '1.2.3',
-             'repository': 'https://lol.de/repo/'},
-            {'name': 'mysql', 'condition': 'mysql.enabled', 'version': '1.2.3', 'repository': 'https://lol.de/repo/'}],
-         'values': [
-             {'name': 'replicaCount', 'description': 'how many replicas to deploy\n', 'default': 1, 'example': ''},
-             {'name': 'image', 'description': 'which image to deploy\n',
-              'default': {'repository': 'nginx', 'pullPolicy': 'IfNotPresent', 'tag': ''},
-              'example': '\nimage:\n  repository: very-doge-wow/stella\n  pullPolicy: IfNotPresent\n  tag: "latest"\n'},
-             {'name': 'imagePullSecrets', 'description': '', 'default': [], 'example': ''},
-             {'name': 'nameOverride', 'description': '', 'default': '', 'example': ''},
-             {'name': 'fullnameOverride', 'description': '', 'default': '', 'example': ''},
-             {'name': 'serviceAccount', 'description': '', 'default': {'create': True, 'annotations': {}, 'name': ''},
-              'example': ''}, {'name': 'podAnnotations', 'description': '', 'default': {}, 'example': ''},
-             {'name': 'podSecurityContext', 'description': '', 'default': {}, 'example': ''},
-             {'name': 'securityContext', 'description': '', 'default': {}, 'example': ''},
-             {'name': 'service', 'description': '', 'default': {'type': 'ClusterIP', 'port': 80}, 'example': ''},
-             {'name': 'ingress', 'description': '', 'default': {'enabled': False, 'className': '', 'annotations': {},
-                                                                'hosts': [{'host': 'chart-example.local', 'paths': [
-                                                                    {'path': '/',
-                                                                     'pathType': 'ImplementationSpecific'}]}],
-                                                                'tls': []}, 'example': ''},
-             {'name': 'resources', 'description': '', 'default': {}, 'example': ''},
-             {'name': 'autoscaling', 'description': '',
-              'default': {'enabled': False, 'minReplicas': 1, 'maxReplicas': 100, 'targetCPUUtilizationPercentage': 80},
-              'example': ''}, {'name': 'nodeSelector', 'description': '', 'default': {}, 'example': ''},
-             {'name': 'tolerations', 'description': '', 'default': [], 'example': ''},
-             {'name': 'affinity', 'description': '', 'default': {}, 'example': ''}],
-         'templates': [{'path': 'deployment.yaml'}, {'path': 'ingress.yaml'}, {'path': 'service.yaml'},
-                       {'path': 'hpa.yaml'}, {'path': 'serviceaccount.yaml'}],
-         'objects': [{'kind': 'Deployment', 'from Template': 'deployment.yaml'},
-                     {'kind': 'Ingress', 'from Template': 'ingress.yaml'},
-                     {'kind': 'Service', 'from Template': 'service.yaml'},
-                     {'kind': 'HorizontalPodAutoscaler', 'from Template': 'hpa.yaml'},
-                     {'kind': 'ServiceAccount', 'from Template': 'serviceaccount.yaml'}],
+        {
+            'name': 'test-chart',
+            'appVersion': '1.16.0',
+            'apiVersion': 'v2',
+            'version': '0.1.0',
+            'description': 'A Helm chart for Kubernetes',
+            'type': 'application',
+            'dependencies': [
+                {'name': 'postgresql', 'condition': 'postgresql.enabled', 'version': '1.2.3',
+                 'repository': 'https://lol.de/repo/'},
+                {'name': 'mysql', 'condition': 'mysql.enabled', 'version': '1.2.3', 'repository': 'https://lol.de/repo/'}
+            ],
+            'values': [
+                {'name': 'replicaCount', 'description': 'how many replicas to deploy\n', 'default': 1, 'example': ''},
+                {'name': 'image', 'description': 'which image to deploy\n', 'default': {'repository': 'nginx', 'pullPolicy': 'IfNotPresent', 'tag': ''}, 'example': '\nimage:\n  repository: very-doge-wow/stella\n  pullPolicy: IfNotPresent\n  tag: "latest"\n'},
+                {'name': 'imagePullSecrets', 'description': '', 'default': [], 'example': ''},
+                {'name': 'nameOverride', 'description': '', 'default': '', 'example': ''},
+                {'name': 'fullnameOverride', 'description': '', 'default': '', 'example': ''},
+                {'name': 'serviceAccount', 'description': '', 'default': {'create': True, 'annotations': {}, 'name': ''}, 'example': ''}, {'name': 'podAnnotations', 'description': '', 'default': {}, 'example': ''},
+                {'name': 'podSecurityContext', 'description': '', 'default': {}, 'example': ''},
+                {'name': 'securityContext', 'description': '', 'default': {}, 'example': ''},
+                {'name': 'service', 'description': '', 'default': {'type': 'ClusterIP', 'port': 80}, 'example': ''},
+                {'name': 'ingress', 'description': '', 'default': {'enabled': False, 'className': '', 'annotations': {}, 'hosts': [{'host': 'chart-example.local', 'paths': [{'path': '/', 'pathType': 'ImplementationSpecific'}]}], 'tls': []}, 'example': ''},
+                {'name': 'resources', 'description': '', 'default': {}, 'example': ''},
+                {'name': 'autoscaling', 'description': '', 'default': {'enabled': False, 'minReplicas': 1, 'maxReplicas': 100, 'targetCPUUtilizationPercentage': 80}, 'example': ''}, {'name': 'nodeSelector', 'description': '', 'default': {}, 'example': ''},
+                {'name': 'tolerations', 'description': '', 'default': [], 'example': ''},
+                {'name': 'affinity', 'description': '', 'default': {}, 'example': ''}],
+            'templates': [
+                {'path': 'deployment.yaml'}, {'path': 'ingress.yaml'}, {'path': 'service.yaml'},
+                {'path': 'hpa.yaml'}, {'path': 'serviceaccount.yaml'}
+            ],
+            'objects': [
+                {'kind': 'Deployment', 'from Template': 'deployment.yaml'},
+                {'kind': 'HorizontalPodAutoscaler', 'from Template': 'hpa.yaml'},
+                {'kind': 'Ingress', 'from Template': 'ingress.yaml'},
+                {'kind': 'Service', 'from Template': 'service.yaml'},
+                {'kind': 'ServiceAccount', 'from Template': 'serviceaccount.yaml'},
+            ],
          'commands': [{'description': '', 'command': ''}]}
     ))
 
