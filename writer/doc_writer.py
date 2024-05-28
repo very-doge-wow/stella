15369b7a0cc19e310980ca853dff1354d4c2578b
fabianfgross <fabian.f.gross@deutschebahn.com>
2024-05-28 17:22:00 +0200

    fix: html lang element

--- writer/doc_writer.py
+++ writer/doc_writer.py
@@ -106,7 +106,7 @@ REPLACE_STRING_BODY
 """
 
     html_advanced_template = r"""<!DOCTYPE html>
-<html lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" lang xml:lang>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
